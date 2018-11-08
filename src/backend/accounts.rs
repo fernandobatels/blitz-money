97fa7cf7e3141e36576a1039a46467b0a325b28a
Luis Fernando Batels <luisfbatels@gmail.com>
2018-11-07 23:13:27 -0200

    Tests: accounts and contacts crud's backend
    Backend of accounts and contacts crud's valideds with tests

--- src/backend/accounts.rs
+++ src/backend/accounts.rs
@@ -130,3 +130,127 @@ impl Account {
         data.remove_by_id(uuid);
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use uuid::Uuid;
+    use chrono::Local;
+
+    fn populate() -> String {
+
+        let path = "/tmp/bmoney-".to_owned() + &Uuid::new_v4().to_string();
+
+        let mut st = Storage { path_str: path.clone(), file: None, lines: Vec::new() };
+
+        assert!(st.start_section("accounts".to_string()));
+
+        let mut data = st.get_section_data("accounts".to_string());
+
+        data.save(Account { uuid: "".to_string(), name: "account 1".to_string(), bank: "bank A".to_string(), currency: "R$".to_string(), open_balance: 0.0, open_balance_date: Some(Local::today().naive_local()) });
+        data.save(Account { uuid: "".to_string(), name: "account 2".to_string(), bank: "bank B".to_string(), currency: "$".to_string(), open_balance: 100.0, open_balance_date: Some(Local::today().naive_local()) });
+        data.save(Account { uuid: "".to_string(), name: "account 3".to_string(), bank: "bank D".to_string(), currency: "$".to_string(), open_balance: -10.0, open_balance_date: Some(Local::today().naive_local()) });
+        data.save(Account { uuid: "".to_string(), name: "account 4".to_string(), bank: "bank B".to_string(), currency: "R$".to_string(), open_balance: 35.0, open_balance_date: Some(Local::today().naive_local()) });
+
+        path
+    }
+
+    #[test]
+    fn get_accounts() {
+
+        let mut st = Storage { path_str: populate(), file: None, lines: Vec::new() };
+
+        let accounts = Account::get_accounts(&mut st);
+
+        assert_eq!(accounts[0].name, "account 4".to_string());
+        assert_eq!(accounts[0].bank, "bank B".to_string());
+        assert_eq!(accounts[0].currency, "R$".to_string());
+        assert_eq!(accounts[0].open_balance, 35.0);
+        assert_eq!(accounts[0].open_balance_date, Some(Local::today().naive_local()));
+
+        assert_eq!(accounts[1].name, "account 3".to_string());
+        assert_eq!(accounts[1].bank, "bank D".to_string());
+        assert_eq!(accounts[1].currency, "$".to_string());
+        assert_eq!(accounts[1].open_balance, -10.0);
+        assert_eq!(accounts[1].open_balance_date, Some(Local::today().naive_local()));
+
+        assert_eq!(accounts[2].name, "account 2".to_string());
+        assert_eq!(accounts[2].bank, "bank B".to_string());
+        assert_eq!(accounts[2].currency, "$".to_string());
+        assert_eq!(accounts[2].open_balance, 100.0);
+        assert_eq!(accounts[2].open_balance_date, Some(Local::today().naive_local()));
+
+        assert_eq!(accounts[3].name, "account 1".to_string());
+        assert_eq!(accounts[3].bank, "bank A".to_string());
+        assert_eq!(accounts[3].currency, "R$".to_string());
+        assert_eq!(accounts[3].open_balance, 0.0);
+        assert_eq!(accounts[3].open_balance_date, Some(Local::today().naive_local()));
+    }
+
+    #[test]
+    fn get_account() {
+
+        let mut st = Storage { path_str: populate(), file: None, lines: Vec::new() };
+
+        let accounts = Account::get_accounts(&mut st);
+
+        let uuid = accounts[0].uuid.clone();
+
+        let account = Account::get_account(&mut st, uuid);
+
+        assert!(account.is_ok());
+        assert_eq!(account.clone().unwrap().name, "account 4".to_string());
+        assert_eq!(account.clone().unwrap().bank, "bank B".to_string());
+        assert_eq!(account.clone().unwrap().currency, "R$".to_string());
+        assert_eq!(account.clone().unwrap().open_balance, 35.0);
+        assert_eq!(account.unwrap().open_balance_date, Some(Local::today().naive_local()));
+        let accounte = Account::get_account(&mut st, "NOOOO".to_string());
+
+        assert!(accounte.is_err());
+    }
+
+
+    #[test]
+    fn store_account() {
+
+        let mut st = Storage { path_str: populate(), file: None, lines: Vec::new() };
+
+        Account::store_account(&mut st, Account { uuid: "".to_string(), name: "account 5".to_string(), bank: "bank A".to_string(), currency: "R$".to_string(), open_balance: 0.0, open_balance_date: Some(Local::today().naive_local())  });
+
+        let accounts = Account::get_accounts(&mut st);
+
+        assert_eq!(accounts[0].name, "account 5".to_string());
+        assert_eq!(accounts[0].bank, "bank A".to_string());
+        assert_eq!(accounts[0].currency, "R$".to_string());
+        assert_eq!(accounts[0].open_balance, 0.0);
+        assert_eq!(accounts[0].open_balance_date, Some(Local::today().naive_local()));
+
+        assert_eq!(accounts[1].name, "account 4".to_string());
+        assert_eq!(accounts[1].bank, "bank B".to_string());
+        assert_eq!(accounts[1].currency, "R$".to_string());
+        assert_eq!(accounts[1].open_balance, 35.0);
+        assert_eq!(accounts[1].open_balance_date, Some(Local::today().naive_local()));
+    }
+
+    #[test]
+    fn remove_account() {
+
+        let mut st = Storage { path_str: populate(), file: None, lines: Vec::new() };
+
+        let accounts = Account::get_accounts(&mut st);
+
+        let uuid = accounts[0].uuid.clone();
+
+        let account = Account::get_account(&mut st, uuid.clone());
+
+        assert!(account.is_ok());
+
+        Account::remove_account(&mut st, uuid.clone());
+
+        let accounte = Account::get_account(&mut st, uuid.clone());
+
+        assert!(accounte.is_err());
+    }
+}
--- src/backend/contacts.rs
+++ src/backend/contacts.rs
@@ -96,3 +96,101 @@ impl Contact {
         data.remove_by_id(uuid);
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use uuid::Uuid;
+
+    fn populate() -> String {
+
+        let path = "/tmp/bmoney-".to_owned() + &Uuid::new_v4().to_string();
+
+        let mut st = Storage { path_str: path.clone(), file: None, lines: Vec::new() };
+
+        assert!(st.start_section("contacts".to_string()));
+
+        let mut data = st.get_section_data("contacts".to_string());
+
+        data.save(Contact { uuid: "".to_string(), name: "contact 1".to_string(), city_location: "city A".to_string() });
+        data.save(Contact { uuid: "".to_string(), name: "contact 2".to_string(), city_location: "city B".to_string() });
+        data.save(Contact { uuid: "".to_string(), name: "contact 3".to_string(), city_location: "city C".to_string() });
+        data.save(Contact { uuid: "".to_string(), name: "contact 4".to_string(), city_location: "city D".to_string() });
+
+        path
+    }
+
+    #[test]
+    fn get_contacts() {
+
+        let mut st = Storage { path_str: populate(), file: None, lines: Vec::new() };
+
+        let contacts = Contact::get_contacts(&mut st);
+
+        assert_eq!(contacts[0].name, "contact 4".to_string());
+        assert_eq!(contacts[0].city_location, "city D".to_string());
+        assert_eq!(contacts[1].name, "contact 3".to_string());
+        assert_eq!(contacts[1].city_location, "city C".to_string());
+        assert_eq!(contacts[2].name, "contact 2".to_string());
+        assert_eq!(contacts[2].city_location, "city B".to_string());
+        assert_eq!(contacts[3].name, "contact 1".to_string());
+        assert_eq!(contacts[3].city_location, "city A".to_string());
+    }
+
+    #[test]
+    fn get_contact() {
+
+        let mut st = Storage { path_str: populate(), file: None, lines: Vec::new() };
+
+        let contacts = Contact::get_contacts(&mut st);
+
+        let uuid = contacts[0].uuid.clone();
+
+        let contact = Contact::get_contact(&mut st, uuid);
+
+        assert!(contact.is_ok());
+        assert_eq!(contact.clone().unwrap().name, "contact 4".to_string());
+        assert_eq!(contact.unwrap().city_location, "city D".to_string());
+
+        let contacte = Contact::get_contact(&mut st, "NOOOO".to_string());
+
+        assert!(contacte.is_err());
+    }
+
+
+    #[test]
+    fn store_contact() {
+
+        let mut st = Storage { path_str: populate(), file: None, lines: Vec::new() };
+
+        Contact::store_contact(&mut st, Contact { uuid: "".to_string(), name: "contact 5".to_string(), city_location: "city E".to_string() });
+
+        let contacts = Contact::get_contacts(&mut st);
+
+        assert_eq!(contacts[0].name, "contact 5".to_string());
+        assert_eq!(contacts[0].city_location, "city E".to_string());
+        assert_eq!(contacts[1].name, "contact 4".to_string());
+        assert_eq!(contacts[1].city_location, "city D".to_string());
+    }
+
+    #[test]
+    fn remove_contact() {
+
+        let mut st = Storage { path_str: populate(), file: None, lines: Vec::new() };
+
+        let contacts = Contact::get_contacts(&mut st);
+
+        let uuid = contacts[0].uuid.clone();
+
+        let contact = Contact::get_contact(&mut st, uuid.clone());
+
+        assert!(contact.is_ok());
+
+        Contact::remove_contact(&mut st, uuid.clone());
+
+        let contacte = Contact::get_contact(&mut st, uuid.clone());
+
+        assert!(contacte.is_err());
+    }
+}
